18296176de19f93f4239c3760865017570632348
milosd92 <milosdanilov@gmail.com>
2016-06-25 19:33:32 +0200

    testing mergesort

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
+#include "mergesort.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    vector<int> unsortedList = {38, 27, 43, 3, 9, 82, 10};
+    vector<int> sortedList;
+
+    cout << "START" << endl;
+
+    for (unsigned int i = 0; i < unsortedList.size(); i++)
+    {
+        cout << unsortedList.at(i) << " ";
+    }
+    cout << endl;
+
+    Mergesort mergesort;
+    sortedList = mergesort.sort(unsortedList);
+
+    for (unsigned int i = 0; i < sortedList.size(); i++)
+    {
+        cout << sortedList.at(i) << " ";
+    }
+    cout << endl;
+
+    cout << "END" << endl;
     return 0;
 }
