6afdc24956fc086e2866cc00e304dc22f6bb0077
milosd92 <milosdanilov@gmail.com>
2016-06-25 20:18:16 +0200

    Defined Constructor and Destructor

--- src/window.cpp
+++ src/window.cpp
@@ -2,4 +2,30 @@
 // Created by danilov on 25.6.16..
 //
 
+#include <SDL2/SDL.h>
 #include "window.h"
+
+Window::Window(unsigned short width, unsigned short height, const std::string &title) {
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    this->sdlWindow = SDL_CreateWindow(title.c_str(),
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       width,
+                                       height,
+                                       SDL_WINDOW_OPENGL
+    );
+
+    this->closed = false;
+}
+
+Window::~Window(void) {
+
+    this->closed = true;
+
+    SDL_DestroyWindow(this->sdlWindow);
+    SDL_Quit();
+
+    delete this;
+}
