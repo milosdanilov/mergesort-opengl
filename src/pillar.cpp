6dce0692ab13a120ea4ac0460c961fbc9d3784df
milosd92 <milosdanilov@gmail.com>
2016-06-26 11:48:07 +0200

    Adding the pillar module

---
+++ src/pillar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by danilov on 26.6.16..
+//
+
+#include "pillar.h"
---
+++ src/pillar.h
@@ -0,0 +1,17 @@
+//
+// Created by danilov on 26.6.16..
+//
+
+#ifndef MERGESORT_OPENGL_PILLAR_H
+#define MERGESORT_OPENGL_PILLAR_H
+
+
+class Pillar
+{
+public:
+protected:
+private:
+};
+
+
+#endif //MERGESORT_OPENGL_PILLAR_H
