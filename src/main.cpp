f931936883cf6c589f8b57ebcc817094bd2684b3
milosd92 <milosdanilov@gmail.com>
2016-07-03 16:07:02 +0200

    Testing execution time of a sorting animation process, with chrono

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <zconf.h>
 #include "mergesort.h"
 
 using namespace std;
 
 int main() {
 
+    pid_t pid = getpid();
+    std::cout << "PID: " << pid << std::endl;
+
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+
     vector<float> unsortedList;
     unsortedList.reserve(100);
 
+    srand ( time(NULL) );
     for (int i = 0; i < 100; i++)
     {
         float height = (float)(rand() % (600 - 200));
@@ -27,7 +35,13 @@ int main() {
     }
 
     Mergesort mergesort(&canvas, &pillars);
-    pillars = mergesort.sort(pillars);
+    pillars = mergesort.sort();
+
+    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+    std::cout << duration << std::endl;
 
     while (! myWindow.isClosed())
     {
